c6c053b552effd61019aade7d41b33d1cd81c882
LookRain <brantscube@hotmail.com>
2017-04-14 18:12:54 +0800

    Day5: more component communication

--- VueJs/Day4/dispatch/main.js
+++ VueJs/Day4/dispatch/main.js
@@ -1,15 +1,46 @@
-Vue.component('coupon', {
-	template: '<input placeholder="enter your code" @blur="onCouponApplied">',
-
-	methods: {
-		onCouponApplied() {
-			this.$emit('applied'); //@applied="onCouponApplied"
-
-			
-		}
+window.Event = new class {
+	constructor() {
+		this.vue = new Vue();
 	}
 
+	fire(event, data = null) {
+		this.vue.$emit(event, data);
+	}
+
+	listen(event, callback) {
+		this.vue.$on(event, callback);
+	}
+}
+
+
+
+Vue.component('coupon', {
+	template: '<input placeholder="enter your code" @blur="onCouponApplied">',
+	methods: {
+		onCouponApplied() {
+			Event.fire('applied'); //@applied="onCouponApplied"
+		}
+	}
 });
+
+Vue.component('notification', {
+    template: `
+	<div v-if="shouldShow" class="notification is-primary">
+        <button class="delete"></button>   
+    </div>
+	`,
+
+	data() {
+		return {
+			shouldShow: false
+		}
+	},
+	created() {
+			Event.listen('applied', () => this.shouldShow = true);
+		}
+})
+
+
 
 new Vue({
 	el: '#root',
@@ -18,10 +49,16 @@ new Vue({
 		couponApplied: false
 	},
 
+	created() {
+		Event.listen('applied', () => alert('handling!'));
+	},
+
 	methods: {
 		onCouponApplied() {
 			this.couponApplied = true;
 		}
-	}
+	},
+
+	
 });
 
