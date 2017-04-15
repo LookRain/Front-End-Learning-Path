4941eb8693482233a0ced4f9c1e8eb821433237e
LookRain <brantscube@hotmail.com>
2017-04-15 18:30:11 +0800

    refactoring

--- VueJs/Day5/oo-forms/public/js/app.js
+++ VueJs/Day5/oo-forms/public/js/app.js
@@ -1,101 +1,126 @@
 class Errors {
-	constructor() {
-		this.errors = {};
-	}
+    constructor() {
+        this.errors = {};
+    }
 
-	get(field) {
-		if (this.errors[field]) {
-			return this.errors[field][0];
-		}
-	}
+    get(field) {
+        if (this.errors[field]) {
+            return this.errors[field][0];
+        }
+    }
 
-	has(field) {
-		return this.errors.hasOwnProperty(field);
-	}
+    has(field) {
+        return this.errors.hasOwnProperty(field);
+    }
 
-	any() {
-		return Object.keys(this.errors).length > 0; //Object.keys() return an array of the keys of the object
+    any() {
+        return Object.keys(this.errors).length > 0; //Object.keys() return an array of the keys of the object
 
-	}
+    }
 
-	record(errors) {
-		this.errors = errors;
-	}
+    record(errors) {
+        this.errors = errors;
+    }
 
-	clear(field) {
-		if (field) {
-			delete this.errors[field];
-			return ;
-		}
+    clear(field) {
+        if (field) {
+            delete this.errors[field];
+            return;
+        }
 
-		this.errors = {};
-	}
+        this.errors = {};
+    }
 }
 
 class Form {
-	constructor(data) {
-		this.originalData = data;
+    /**
+     * Create a new Form instance.
+     *
+     * @param {object} data
+     */
+    constructor(data) {
+        this.originalData = data;
 
-		for (let field in data) {
-			this[field] = data[field];
-		}
+        for (let field in data) {
+            this[field] = data[field];
+        }
 
-		this.errors = new Errors();
-	}
+        this.errors = new Errors();
+    }
 
-	data() {
-		let data = Object.assign({}, this); 
-		delete data.originalData;
-		delete data.errors;
-		// console.log('look here');
-		// console.log(data);
-		return data;
-	}
+    data() {
+    	let data = {};
 
-	reset() {
-		for (let field in this.originalData) {
-			this[field] = '';
-		}
-		this.errors.clear();
-	}
+    	for (let property in this.originalData) {
+            data[property] = this[property];
+        }
+        // let data = Object.assign({}, this);
+        // delete data.originalData;
+        // delete data.errors;
+        // console.log('look here');
+        // console.log(data);
+        return data;
+    }
 
-	submit(requestType, url) {
-		// console.log(this.$data.form.data);
-		axios[requestType](url, this.data())
-		.then(this.onSuccess.bind(this))
-		.catch(this.onFail.bind(this));
+    reset() {
+        for (let field in this.originalData) {
+            this[field] = '';
+        }
+        this.errors.clear();
 
-	}
+    }
 
-	onSuccess(response) {
-		alert(response.data.message);
-		
-		this.reset();
+    post(url) {
+    	return this.submit('post', url);
+    }
 
-	}
+    submit(requestType, url) {
+        return new Promise((resolve, reject) => {
+            axios[requestType](url, this.data())
+                .then(response => {
+                    this.onSuccess(response.data);
 
-	onFail(error) {
-		this.errors.record(error.response.data);
-		// console.log(error);
-	}
+                    resolve(response.data);
+                })
+                .catch(error => {
+                    this.onFail(error.response.data);
+
+                    reject(error.response.data);
+                });
+        });
+    }
+
+    onSuccess(data) {
+        alert(data.message);
+        
+        this.reset();
+
+    }
+
+    onFail(errors) {
+        this.errors.record(errors);
+        // console.log(error);
+    }
 }
 
 new Vue({
-	el: '#app',
+    el: '#app',
 
-	data: {
-		form: new Form({
-			title: '',
-			description: ''
-		})		
-	},
+    data: {
+        form: new Form({
+            title: '',
+            description: ''
+        })
+    },
 
 
 
-	methods: {
-		onSubmit() {
+    methods: {
+        onSubmit() {
 
-			this.form.submit('post', '/projects');
-		}
-	}
+            this.form.post('/projects')
+            .then(data => console.log(data))
+            .catch(errors => console.log(errors));
+        }
+    }
 });
