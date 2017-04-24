c4ab6826f4999495f3976cb6e1156dbfb9acfa8e
LookRain <brantscube@hotmail.com>
2017-04-25 00:07:25 +0800

    correct implementation of custom input component

--- VueJs/Day10/custom-input-component/main.js
+++ VueJs/Day10/custom-input-component/main.js
@@ -1,27 +1,57 @@
-Vue.component('coupon', {
-	props: ['code'],
+Vue.component('coupon', { 
+	model: {
+        prop: 'code',
+        event: 'input'
+    },
+    props: ['code'],
 
-	template: `
-		<input class="input" :value="code" @input="updateCode($event.target.value)">
+    data(){
+    	return {
+    		validCodes: ['3154', '666', '999']
+    	}
+    	
+    },
+
+    template: `
+		<input class="input" :value="code" @input="updateCode($event.target.value)" ref="input">
 	`,
 
-	methods: {
-		updateCode(code) {
-			this.$emit('input', code);
-		}
-	}
+    methods: {
+        updateCode(code) {
+        	if (this.validCodes.includes(code)) {
+        		alert('applied!!!!');
+
+        		this.$refs.input.value = code = '';
+
+        	}
+
+            this.$emit('input', code);
+        }
+    },
+    
 
 });
 
+/*
+Using :value="code" in my component did not work for me. In the Vue docs, section "Form Input Components using Custom Events", it states for a component to work with v-model, it needs (specifically starting with v 2.2):
+
+1) Accept 'value' prop
+2) emit an input event with a new value
+
+So in my code I changed props: ['code'] to props: ['value']
+
+Also changed :value="code" to :value="value"
+*/
 
 new Vue({
-	el: '#root',
-	data: {
-		coupon: 'coupon is here'
-	},
-	// methods: {
-	// 	updateCoupon(input) {
-	// 		this.coupon = input;
-	// 	}
-	// }
+    el: '#root',
+    data: {
+        coupon: 'coupon is here'
+    },
+
+    // methods: {
+    // 	updateCoupon(input) {
+    // 		this.coupon = input;
+    // 	}
+    // }
 });
