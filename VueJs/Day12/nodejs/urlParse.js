51d71ac6312069c5c7f5fe294a6f80021522ae58
YU LU <brantscube@hotmail.com>
2017-05-01 07:02:26 -0700

    Parse url

---
+++ VueJs/Day12/nodejs/urlParse.js
@@ -0,0 +1,13 @@
+var url = require('url');
+
+var addr = 'http://localhost:8080/default.htm?year=2017&month=february';
+
+var q = url.parse(addr, true);
+
+console.log(q.host);
+console.log(q.pathname);
+console.log(q.search);
+
+var qdata = q.query;
+
+console.log(qdata);
