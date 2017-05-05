f95ab93e5b5304d019e4aa8e735bbfbc9ae723ff
LookRain <brantscube@hotmail.com>
2017-05-05 23:40:49 +0800

    read and write stream

--- VueJs/Day12/nodejs/stream/main.js
+++ VueJs/Day12/nodejs/stream/main.js
@@ -8,16 +8,21 @@ var readerStream = fs.createReadStream('input.txt');
 readerStream.setEncoding('UTF8');
 
 // 处理流事件 --> data, end, and error
-readerStream.on('data', function(chunk) {
-   data += chunk;
-});
+readerStream.on('data', chunk => data += chunk);
 
-readerStream.on('end',function(){
-   console.log(data);
-});
+readerStream.on('end',() => console.log(data));
 
-readerStream.on('error', function(err){
-   console.log(err.stack);
-});
+readerStream.on('error', err => console.log(err.stack));
 
 console.log("程序执行完毕");
+
+// write
+
+let writerStream = fs.createWriteStream('output.txt');
+let text = "nothing goes here...";
+writerStream.write(text, 'UTF8');
+writerStream.end();
+writerStream.on('finish', () => console.log('finished writing'));
+writerStream.on('error', err => console.log(err.stack));
+
+writerStream.end();
