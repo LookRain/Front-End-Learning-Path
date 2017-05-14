f40f8752bcd2573702e54f096a6be23ce0ef1fc3
LookRain <brantscube@hotmail.com>
2017-05-14 21:43:37 +0800

    Vuex counter app

---
+++ VueJs/Day13/hello-vue/src/Counter.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    Clicked: {{ $store.state.count }} times, count is {{ evenOrOdd }}.
+    <button @click="increment">+</button>
+    <button @click="decrement">-</button>
+    <button @click="incrementIfOdd">Increment if odd</button>
+    <button @click="incrementAsync">Increment async</button>
+    <br>
+    {{ $store.state.count }}
+  </div>
+</template>
+
+<script>
+import { mapGetters, mapActions } from 'vuex'
+export default {
+  computed: mapGetters([
+    'evenOrOdd'
+  ]),
+  methods: mapActions([
+    'increment',
+    'decrement',
+    'incrementIfOdd',
+    'incrementAsync'
+  ])
+}
+</script>
