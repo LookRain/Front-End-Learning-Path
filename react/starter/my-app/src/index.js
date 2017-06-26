398aa68ab417000da41e9acfb59ae3b86f42fc07
LookRain <brantscube@hotmail.com>
2017-06-26 23:37:28 +0800

    passing data using props

--- react/starter/my-app/src/index.js
+++ react/starter/my-app/src/index.js
@@ -9,7 +9,7 @@ class Square extends React.Component {
   render() {
     return (
       <button className="square">
-        {/* TODO */}
+        {this.props.value}
       </button>
     );
   }
@@ -17,7 +17,7 @@ class Square extends React.Component {
 
 class Board extends React.Component {
   renderSquare(i) {
-    return <Square />;
+    return <Square value={i} />;
   }
 
   render() {
