517e35c3bf7594539a1c6f63979645de48e2d694
LookRain <brantscube@hotmail.com>
2017-06-26 23:32:04 +0800

    learning react

---
+++ react/starter/my-app/src/Game.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Game extends React.Component {
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
+  }
+}
