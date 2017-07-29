dfea82177cd279b83cca39d65bbb96dfeffec956
LookRain <brantscube@hotmail.com>
2017-07-30 02:34:37 +0800

    add state to component Square and handle click event

--- react/starter/my-app/src/Square.js
+++ react/starter/my-app/src/Square.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react';
 
 export default class Square extends React.Component {
-  render() {
+  constructor () {
+    super()
+    this.state = {
+      value: null,
+    }
+  }
+
+  render () {
     return (
-      <button className="square">
-        {this.props.value}
+      <button className="square" onClick={
+        () => {
+          this.setState({value: 'X'})
+        }
+      }>
+        {this.state.value}
       </button>
     );
   }
