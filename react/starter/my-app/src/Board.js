806359c984dc24a0b6722747073e0322d81bc16c
LookRain <brantscube@hotmail.com>
2017-07-30 16:37:38 +0800

    add winner

--- react/starter/my-app/src/Board.js
+++ react/starter/my-app/src/Board.js
@@ -33,8 +33,18 @@ export default class Board extends React.Component {
     })
   }
 
+
+
   render() {
-    const status = `Next player: ${(this.state.xNext ? 'X' : 'O')}`
+    const winner = calculateWinner(this.state.squares)
+    console.log(winner)
+    let status
+    if (winner) {
+      status = `Winner is: ${winner}!`
+    } else {
+      status = `Next player: ${(this.state.xNext ? 'X' : 'O')}`
+    }
+
 
     return (
       <div>
@@ -59,3 +69,24 @@ export default class Board extends React.Component {
     );
   }
 }
+
+  function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+
+    return null;
+  }
