6f93c889cab6923b0869c2cd71bf7a8cd92888f2
LookRain <brantscube@hotmail.com>
2017-08-02 22:17:09 +0800

    loop

--- react/follow-doc/2/1.js
+++ react/follow-doc/2/1.js
@@ -2,4 +2,14 @@ function Greet(props) {
   return <h1>Hi, {props.name}</h1>
 }
 
-ReactDOM.render((<Greet name="LookRain" />), document.getElementById('root'))
+function App(props) {
+  let i
+  let result = []
+  for (i = 0; i < 10; i++) {
+    result.push(<h1 key={i}>Hi, {props.name}</h1>)
+  }
+  return <div>{result}</div>
+}
+
+// ReactDOM.render((<Greet name="LookRain" />), document.getElementById('root'))
+ReactDOM.render((<App time={10} name="LookRain"/>), document.getElementById('root'))
