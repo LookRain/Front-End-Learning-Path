0b475ca36de9fc3828701df56c8a540d7d3c463c
LookRain <brantscube@hotmail.com>
2017-08-04 23:18:58 +0800

    experimenting with vscode and eslint

--- react/follow-doc/test.js
+++ react/follow-doc/test.js
@@ -1,8 +1,8 @@
 function Greet(props) {
-  return <h1>Hi, {props.name}</h1>
+  return <h1>Hi, {props.name}</h1>;
 }
 
-function App(props) {  
+function App(props) {
   // let i
   // let result = []
   // for (i = 0; i < 10; i++) {
@@ -10,27 +10,26 @@ function App(props) {
   // }
   // return <div>{result}</div>
 
-  
   return (
     <div>
-    {props.numbers.map(number=>
-      <h1>Hola, {props.name}!</h1>
+      {props.numbers.map(number =>
+        <h1>Hola, {props.name}!</h1>,
       )}
     </div>
-    );
+  );
 }
 
 function NumberList(props) {
-    const numbers = props.numbers;
+  const numbers = props.numbers;
   return (
     <ul>
-         {numbers.map((number) =>
-           <li key={number.toString()}>{number}</li>
-         )}
+      {numbers.map(number =>
+        <li key={number.toString()}>{number}</li>,
+      )}
     </ul>
   );
 }
 
 // ReactDOM.render((<Greet name="LookRain" />), document.getElementById('root'))
-ReactDOM.render((<App time={10} numbers={3} name="LookRain"/>), document.getElementById('root'))
+ReactDOM.render((<App time={10} numbers={3} name="LookRain" />), document.getElementById('root'));
 // ReactDOM.render((<NumberList numbers={[1,2,3,5]}/>), document.getElementById('root'))
