d2cd35a9d53fddda747de88d58c5c02c328e27ce
LookRain <brantscube@hotmail.com>
2017-08-08 00:14:29 +0800

    linting md

--- react/follow-doc/4/props/src/Clock.js
+++ react/follow-doc/4/props/src/Clock.js
@@ -1,8 +1,12 @@
 import React, { Component } from 'react';
 
-function Clock() {
-  
-  }
+function Clock(props) {
+  return (
+    <div>
+      <h1>Hello, world!</h1>
+      <h2>It is {props.date.toLocaleTimeString()}.</h2>
+    </div>
+  );
 }
 
 export default Clock;
