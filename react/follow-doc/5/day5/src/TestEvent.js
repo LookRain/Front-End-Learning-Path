43c6e0b0f00b646a74094a8230a423a5358e98bb
LookRain <brantscube@hotmail.com>
2017-08-08 22:15:11 +0800

    remember to bind function to 'this' for 'this' to work in functions

--- react/follow-doc/5/day5/src/TestEvent.js
+++ react/follow-doc/5/day5/src/TestEvent.js
@@ -1,16 +1,31 @@
 import React, { Component } from 'react'
 
 class TestEvent extends Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      status: true
+    }
+    // MUST USE BIND to make `this` work in the callback
+    this.handleClick = this.handleClick.bind(this)
+  }
+
   render () {
     return (
-      <a href="#" onClick={this.handleClick}>Click me</a>
+      <div><p>{this.state.status ? 'On' : 'Off'}</p>
+      <button onClick={this.handleClick}>Toggle</button>
+      </div>
     )
   }
 
   // Use preventDefault
-  handleClick (e) {
-    e.preventDefault()
-    alert('lalala')
+  handleClick () {
+    this.setState(prevState => (
+      {
+        status: !prevState.status
+      }
+    ))
   }
 }
 
