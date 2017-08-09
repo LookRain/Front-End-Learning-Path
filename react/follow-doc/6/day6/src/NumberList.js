175ce4f15528a9af9cfc1f5583002593b218657e
LookRain <brantscube@hotmail.com>
2017-08-09 23:51:00 +0800

    basic list rendering, key must be string

---
+++ react/follow-doc/6/day6/src/NumberList.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+function NumberList (props) {
+  const numbers = props.numbers
+
+  const listItems = numbers.map(number => <li key={number.toString()}>{number}</li>)
+
+  return <ul>{listItems}</ul>
+}
+
+export default NumberList
--- react/follow-doc/6/day6/src/index.js
+++ react/follow-doc/6/day6/src/index.js
@@ -4,11 +4,12 @@ import './index.css'
 import CounterApp from './CounterApp'
 import Greeting from './Greeting'
 import LoginControl from './LoginControl'
+import NumberList from './NumberList'
 import registerServiceWorker from './registerServiceWorker'
 
 ReactDOM.render(
   <div>
-    <LoginControl></LoginControl>
+    <NumberList numbers={[1, 2, 3, 4, 5]}></NumberList>
   </div>,
 document.getElementById('root'))
 registerServiceWorker()
