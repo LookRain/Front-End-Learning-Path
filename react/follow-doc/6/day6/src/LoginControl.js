12d56bebdf151a32c719294f042322336e7cb33a
LookRain <brantscube@hotmail.com>
2017-08-09 18:41:51 +0800

    inline if-else conditional rendering

--- react/follow-doc/6/day6/src/LoginControl.js
+++ react/follow-doc/6/day6/src/LoginControl.js
@@ -17,21 +17,37 @@ class LoginControl extends Component {
 
   render () {
     const isLoggedIn = this.state.isLoggedIn
+    /*
+      Approach 1: Basic Conditional Rendering
+    */
+    // let button = null
+    // if (isLoggedIn) {
+    //   button = <LogoutButton onClick={this.handleLogoutClick}></LogoutButton>
+    // } else {
+    //   button = <LoginButton onClick={this.handleLoginClick}></LoginButton>
+    // }
 
-    let button = null
-    if (isLoggedIn) {
-      button = <LogoutButton onClick={this.handleLogoutClick}></LogoutButton>
-    } else {
-      button = <LoginButton onClick={this.handleLoginClick}></LoginButton>
-    }
+    // return (
+    //   <div>
+    //     {button}
+    //     <Greeting isLoggedIn={isLoggedIn} />>
 
+    //   </div>
+    // )
+
+    /*
+      Approach 2: Inline if else
+    */
     return (
       <div>
-        {button}
-        <Greeting isLoggedIn={isLoggedIn}>
-
-        </Greeting>
-
+        <Greeting isLoggedIn={isLoggedIn} />
+        {
+          isLoggedIn ? (
+            <LogoutButton onClick={this.handleLogoutClick}></LogoutButton>
+          ) : (
+            <LoginButton onClick={this.handleLoginClick}></LoginButton>
+          )
+        }
       </div>
     )
   }
