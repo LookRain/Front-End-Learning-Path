b887b9d65a8837149481dcb7142fc9fdfbc78094
LookRain <brantscube@hotmail.com>
2017-08-12 23:42:47 +0800

    add styling

--- react/follow-doc/6/7/day7/src/Todo.js
+++ react/follow-doc/6/7/day7/src/Todo.js
@@ -9,10 +9,12 @@ class Todo extends Component {
   }
   render () {
     return (
-      <div>
-        <Header />
-        {this.state.tasks.map(task => <Task task={task}/>)}
-      </div>
+      <section className="section">
+        <div className="container has-text-centered">
+          <Header />
+          {this.state.tasks.map(task => <Task task={task}/>)}
+        </div>
+      </section>
 
     )
   }
@@ -25,14 +27,16 @@ class Task extends Component {
 
   render () {
     return (
-      <p>{this.props.task}</p>
+      <div>
+        <p className="notification">{this.props.task}</p>
+      </div>
     )
   }
 }
 
 function Header () {
   return (
-    <section className="hero is-primary">
+    <section className="hero is-warning">
       <div className="hero-body">
         <div className="container has-text-centered">
           <h1 className="title">
