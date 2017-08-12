b23c2621d9ac95bbbe7b5f29062fbc61dfb2bc09
LookRain <brantscube@hotmail.com>
2017-08-12 22:17:06 +0800

    basic structure

--- react/follow-doc/6/7/day7/src/App.js
+++ react/follow-doc/6/7/day7/src/App.js
@@ -1,21 +1,22 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+// import logo from './logo.svg'
+// import './App.css'
 
 class App extends Component {
-  render() {
+  render () {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <section className="hero is-primary">
+        <div className="hero-body">
+          <div className="container">
+            <h1 className="title" style={{fontWeight: 300}}>
+              r-Todo
+            </h1>
+          </div>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+
+      </section>
+    )
   }
 }
 
-export default App;
+export default App
