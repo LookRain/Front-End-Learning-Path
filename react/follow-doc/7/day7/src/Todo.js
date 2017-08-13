d104439ea5a6ad0a88996776afe4465463692341
LookRain <brantscube@hotmail.com>
2017-08-14 00:28:55 +0800

    update

--- react/follow-doc/7/day7/src/Todo.js
+++ react/follow-doc/7/day7/src/Todo.js
@@ -13,7 +13,7 @@ class Todo extends Component {
     let tempTasks = this.state.tasks.slice()
     tempTasks.splice(id, 1)
 
-    let tempFinished = this.state.tasks.slice()
+    let tempFinished = this.state.finished.slice()
     tempFinished.push(t)
     this.setState({
       // finished: this.state.finished.slice().push(t),
@@ -24,19 +24,38 @@ class Todo extends Component {
   }
   render () {
     return (
-      <section className="section">
-        <div className="container has-text-centered">
-          <Header />
-          {this.state.tasks.map((task, id) => <Task index={id} key={task}>
-            <div onClick={() => { this.markComplete(id) }}>{task}</div>
-          </Task>)}
-          <hr/>
-          {this.state.finished.map((task, id) => <FinishedTask index={id} key={task}>
-            <div onClick={() => { this.markComplete(id) }}>{task}</div>
-          </FinishedTask>)}
-        </div>
-      </section>
+      <div>
+        <Header />
+        <section className="section">
+          <div className="container has-text-centered">
+            <TaskInput/>
+            <TaskHeader />
+            {this.state.tasks.map((task, id) => <Task index={id} key={task}>
+              <div onClick={() => { this.markComplete(id) }}>{task}</div>
+            </Task>)}
 
+            <hr/>
+            <FinishedHeader/>
+            {this.state.finished.map((task, id) => <FinishedTask index={id} key={task}>
+              <div onClick={() => {}}>{task}</div>
+            </FinishedTask>)}
+          </div>
+        </section>
+      </div>
+
+    )
+  }
+}
+
+class TaskInput extends Component {
+  state = { }
+  render () {
+    return (
+      <div className="columns">
+        <div className="column is-8"><TextInput></TextInput></div>
+        <div className="column is-2"><div className="button is-primary">Add</div></div>
+        <div className="column is-2"><div className="button is-danger">Clear All</div></div>
+      </div>
     )
   }
 }
@@ -76,17 +95,40 @@ class FinishedTask extends Component {
 
 function Header () {
   return (
-    <section className="hero is-warning">
+    <section className="hero is-primary">
       <div className="hero-body">
         <div className="container has-text-centered">
           <h1 className="title">
-              r-Todo
+              R-todo
           </h1>
-          <h2 className="subtitle">a todo app based on Vue.js</h2>
+          <h2 className="subtitle">a todo app based on React.js</h2>
         </div>
       </div>
 
     </section>
+  )
+}
+
+class TextInput extends Component {
+  state = {
+
+  }
+  render () {
+    return (
+      <input type="text" className="input is-primary" />
+    )
+  }
+}
+
+function TaskHeader () {
+  return (
+    <div className="notification is-success">Current Tasks:</div>
+  )
+}
+
+function FinishedHeader () {
+  return (
+    <div className="notification is-warning">Finished Taks:</div>
   )
 }
 
