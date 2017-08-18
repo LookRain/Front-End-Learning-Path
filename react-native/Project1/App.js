806d97dfcdf80c9bb9fa1a96780de5d63f7dd218
LookRain <brantscube@hotmail.com>
2017-08-18 19:19:51 +0800

    basic react native

---
+++ react-native/Project1/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <Test></Test>
+    );
+  }
+}
+
+class Test extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>hello world!</Text>
+
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
