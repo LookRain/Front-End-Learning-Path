debf836d79519a632f288589cca50be2a1bf23dc
LookRain <brantscube@hotmail.com>
2017-08-19 01:53:54 +0800

    Adding alignItems to a component's style determines the alignment of children along the secondary axis (if the primary axis is row, then the secondary is column, and vice versa)

--- react-native/Project1/App.js
+++ react-native/Project1/App.js
@@ -4,10 +4,31 @@ import { StyleSheet, Text, View, Image } from 'react-native';
 export default class App extends React.Component {
   render() {
     return (
-      <JustifyContentBasics></JustifyContentBasics>
+      <AlignItemsBasics></AlignItemsBasics>
     );
   }
 }
+
+class AlignItemsBasics extends Component {
+  render() {
+    return (
+      // Try setting `alignItems` to 'flex-start'
+      // Try setting `justifyContent` to `flex-end`.
+      // Try setting `flexDirection` to `row`.
+      <View style={{
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'flex-end',
+        //Available options are flex-start, center, flex-end, and stretch
+      }}>
+        <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
+        <View style={{width: 50, height: 50, backgroundColor: 'skyblue'}} />
+        <View style={{width: 50, height: 50, backgroundColor: 'steelblue'}} />
+      </View>
+    );
+  }
+};
 
 class FixedDimensionsBasics extends Component {
   render() {
