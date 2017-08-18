6ff1c8f930e45aea20087e14f620e4f1cff0934b
LookRain <brantscube@hotmail.com>
2017-08-19 00:58:27 +0800

    blink blink

--- react-native/Project1/App.js
+++ react-native/Project1/App.js
@@ -1,10 +1,12 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default class App extends React.Component {
   render() {
     return (
-      <Test></Test>
+      <Bananas>
+        some bananas
+      </Bananas>
     );
   }
 }
@@ -17,6 +19,56 @@ class Test extends React.Component {
 
       </View>
     )
+  }
+}
+
+class Bananas extends Component {
+
+  render() {
+    let pic = {
+      uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
+    }
+    return (
+      <View style={{flex: 1,alignItems: 'center',justifyContent: 'center'}}>
+        <BlinkApp></BlinkApp>
+        <Text>{this.props.children}</Text>
+      <Image source={pic} style={{width: 193, height: 110}}></Image>
+      </View>
+    )
+  }
+}
+
+class Blink extends Component {
+  state = {
+    showText : true
+  }
+
+  componentDidMount() {
+    setInterval(() => {
+      this.setState(previousState => {
+        return { showText: !previousState.showText };
+      });
+    }, 1000);
+  }
+
+  render() {
+    let display = this.state.showText ? this.props.text : ' ';
+    return (
+      <Text>{display}</Text>
+    );
+  }
+}
+
+class BlinkApp extends Component {
+  render() {
+    return (
+      <View>
+        <Blink text='I love to blink' />
+        <Blink text='Yes blinking is so great' />
+        <Blink text='Why did they ever take this out of HTML' />
+        <Blink text='Look at me look at me look at me' />
+      </View>
+    );
   }
 }
 
